cdd26c057ece0f57070ee21fc7a5eef9dc3992dc
weldocarvalho <weldocarvalho@outlook.com>
2021-10-18 22:43:17 -0300

    git oauth, dotenv and prisma add

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
@@ -8,6 +9,13 @@ app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`),
 );
 
-app.get('/', (req, res) => {
-  res.send('alive');
+app.get('/github', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
 });
