5474088d31155c0ef44d1b3e0ece409a7e647732
weldocarvalho <weldocarvalho@outlook.com>
2021-10-18 23:34:50 -0300

    userInfo received; user schema and migration add

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,21 +1,62 @@
 import axios from 'axios';
+import prismaClient from '../prisma_db';
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
 
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const { data } = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+    const { data } = await axios.get<IUserResponse>(
+      'https://api.github.com/user',
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
       },
-      headers: {
-        Accept: 'application/json',
+    );
+
+    const { login, id, avatar_url, name } = data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
       },
     });
 
-    return data;
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    return user;
   }
 }
 
