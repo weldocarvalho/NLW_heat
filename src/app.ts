5f82f38de0c5635573fe1edca0c362aa89cd3bbd
weldocarvalho <weldocarvalho@outlook.com>
2021-10-18 22:23:36 -0300

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 4000;
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`),
+);
+
+app.get('/', (req, res) => {
+  res.send('alive');
+});
